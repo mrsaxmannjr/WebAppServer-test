e322fb65f0c4f75a04dab86e6741ca393da8ccd8
mrsaxmannjr <mrsaxmannjr@gmail.com>
2017-12-15 14:24:00 -0700

    first commit

--- Data/data.js
+++ Data/data.js
@@ -1,8 +1,53 @@
-var dataSet = [{
-  id: 1,
-  name: "James",
-}];
+var bios = [
+  {
+    name: "Goku",
+    race: "Saiyan"
+  },
+  {
+    name: "Vegeta",
+    race: "Saiyan"
+  }
+];
+
+var stats = [
+  {
+    name: "Goku",
+    strength: 9,
+    specialMoves: [],
+    combos: []
+  },
+  {
+    name: "Vegeta",
+    strength: 10,
+    specialMoves: [],
+    combos: []
+  },
+  {
+    name: "Krillin",
+    strength: 5,
+    specialMoves: [],
+    combos: []
+  }
+];
+
+// function combine(data1, data2) {
+//   var result = [];
+//   for (var i = 0; i < data1.length; i++) {
+//     result.push(data1[i]);
+//     for (var j = 0; j < data2.length; j++) {
+//       if (result[i].name === data2[j].name) {
+//         result[i].strength = data2[j].strength
+//         result[i].specialMoves = data2[j].specialMoves
+//         result[i].combos = data2[j].combos
+//       }
+//     }
+//   }
+//   return result;
+// }
+//
+// console.log(combine(bios, stats));
 
 module.exports = {
-  dataSet
+  bios,
+  stats
 };
--- app.js
+++ app.js
@@ -3,12 +3,30 @@ const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
 const data = require("./Data/data");
+const bios = data.bios
+const stats = data.stats
+var result = [];
+
+function combine(data1, data2) {
+  for (var i = 0; i < data1.length; i++) {
+    result.push(data1[i]);
+    for (var j = 0; j < data2.length; j++) {
+      if (result[i].name === data2[j].name) {
+        result[i].strength = data2[j].strength
+        result[i].specialMoves = data2[j].specialMoves
+        result[i].combos = data2[j].combos
+      }
+    }
+  }
+}
+combine(bios, stats);
+
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (request, response) => {
-  response.json(data.dataSet[0].name);
+  response.json(result);
 });
 
 app.post("/", (request, response) => {
